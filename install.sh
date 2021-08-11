6d2765e5313cb289ce28314f1a0373e5e4297176
Pradeep S <75474807+iamprady@users.noreply.github.com>
2021-08-11 16:28:25 +0530

    modify zsh aliases

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@
 
 #zsh
 [ ! -f $HOME/.zshenv ] && ln -s $HOME/Repos/Dotfiles/.zshenv $HOME/.zshenv
-[ ! -d $HOME/.config/zsh ] && ln -s $HOME/Repos/Dotfiles/.config/zsh/ $HOME/.config/zsh/
+[ ! -d $HOME/.config/zsh ] && ln -s $HOME/Repos/Dotfiles/.config/zsh $HOME/.config/zsh
+
+#vim
+[ ! -f $HOME/.vimrc ] && ln -s $HOME/Repos/Dotfiles/.vimrc $HOME/.vimrc
 
 #starship prompt
 [ ! -f $HOME/.config/starship.toml ] && ln -s $HOME/Repos/Dotfiles/.config/starship.toml $HOME/.config/starship.toml
